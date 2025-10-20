3696cd612dfbf29961b8c871e4af25d5e663dbb6
1911guy <dev@[200~mercury~.vl>
2025-10-20 16:30:59 +0600

    claer deps

--- src/app/_providers/appProvider.tsx
+++ src/app/_providers/appProvider.tsx
@@ -3,14 +3,10 @@
 import { BluetoothProvider } from '@front/entities/bluetooth-device/context'
 import ThemeProvider from '@front/kernel/lib/next-theme/themeProvider'
 
-import QueriesClient from './queryClient'
-
 const AppProvider = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<ThemeProvider>
-			<QueriesClient>
-				<BluetoothProvider>{children}</BluetoothProvider>
-			</QueriesClient>
+			<BluetoothProvider>{children}</BluetoothProvider>
 		</ThemeProvider>
 	)
 }
